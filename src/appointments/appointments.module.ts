ec543cb5ce13408d83eefd4b79744e6726f7090b
Shwetalbhargav <hridaan.purav@gmail.com>
2026-01-11 06:36:44 +0530

    all prisam module error removed

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppointmentsController } from './appointments.controller';
 import { AppointmentsService } from './appointments.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { RescheduleOffersModule } from '../reschedule-offers/reschedule-offers.module';
 
 @Module({
+  imports: [PrismaModule,RescheduleOffersModule,],
   controllers: [AppointmentsController],
-  providers: [AppointmentsService, PrismaService],
+  providers: [AppointmentsService],
   exports: [AppointmentsService],
 })
 export class AppointmentsModule {}
